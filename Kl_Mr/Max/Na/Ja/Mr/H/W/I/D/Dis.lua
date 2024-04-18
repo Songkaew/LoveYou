8b456e6b0207ae86f60bb34f01675155204237d7
Songkaew <165384050+Songkaew@users.noreply.github.com>
2024-04-18 16:50:42 +0700

    Create Dis.lua

---
+++ Kl_Mr/Max/Na/Ja/Mr/H/W/I/D/Dis.lua
@@ -0,0 +1,43 @@
+
+local UserPy = game.Players.LocalPlayer.Name
+local ClientId = game:GetService("RbxAnalyticsService"):GetClientId()
+local whitelist = string.split((ClientId), '-')
+local Hxven = whitelist[5]
+
+if register_key == Hxven then
+    if whitelist[register_key] == game:GetService("RbxAnalyticsService"):GetClientId() then
+        print("whitelist![1]")
+    else
+        print("whitelist![2]")
+    end
+else
+    print("รอแอดมินแอด whitelist")
+    setclipboard("_G.register_key = "..(Hxven).."\n whitelist : "..(ClientId))
+    local url = "https://discord.com/api/webhooks/1229353612673683508/cm2V78tpwwH5qUt6AQ9a-py5Tez4foSYvU9QL60V-ekV83ZewSG1dt4y4QTL9sM4yPNV" -- ur webhook url
+    local data = { 
+        ["username"] = 'Key | MrMaxNaJa', -- Webhook name here
+        ["avatar_url"] = "https://cdn.discordapp.com/attachments/948603231192363058/1088077196997955704/Untitled-1_copy.png", -- ur discord logo url
+        ["embeds"] = {
+            {
+                ["description"] = "ผู้เล่น : "..UserPy.." ใช้งานแล้ว",
+                ["color"] = tonumber(0x00ff00), -- color id		
+                ["type"] = "rich",
+                ["fields"] =  {
+                    {
+                        ["name"] = "[📁] Key เอาไป Add ใส่ในสคริป",
+                        ["value"] = "_G.register_key = "..(Hxven).."\n `whitelist : "..(ClientId).."`\n `register_key = "..(Hxven).."`",
+                    }
+                },
+                ["footer"] = {
+                    ["text"] = "YouTube:MrMaxNaJa"
+                },
+                ["timestamp"] = DateTime.now():ToIsoDate(),
+            }
+        },
+    } 
+    local newdata = game:GetService("HttpService"):JSONEncode(data)
+    local headers = {["content-type"] = "application/json"}
+    request = http_request or request or HttpPost or syn.request
+    local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+    request(abcdef)
+end
