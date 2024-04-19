9fb1e9b7f00765bf31475a5c411fcc0fc1ce7373
Songkaew <165384050+Songkaew@users.noreply.github.com>
2024-04-19 20:52:39 +0700

    Update Dis.lua

--- Kl_Mr/Max/Na/Ja/Mr/H/W/I/D/Dis.lua
+++ Kl_Mr/Max/Na/Ja/Mr/H/W/I/D/Dis.lua
@@ -12,20 +12,21 @@ if register_key == Hxven then
     end
 else
     print("รอแอดมินแอด whitelist")
-    setclipboard("_G.register_key = "..(Hxven).."\n whitelist : "..(ClientId))
+    setclipboard('_G.register_key = "'..(Hxven)..'"\n loadstring(game:HttpGet("https://raw.githubusercontent.com/xOne2/Test/main/README.md"))() \n whitelist : '..(ClientId))
+
     local url = "https://discord.com/api/webhooks/1230867652357128235/1l1VW3AXOGxnajxhTRt0C6Sczwyp_tRw20VfQ-pDQb1XiBI0r0EsYPICpc1krk2G0SFI" -- ur webhook url
     local data = { 
-        ["username"] = 'Key | MrMaxNaJa', -- Webhook name here
+        ["username"] = 'Key Hwid | By MrMaxNaJa', -- Webhook name here
         ["avatar_url"] = "https://cdn.discordapp.com/attachments/948603231192363058/1088077196997955704/Untitled-1_copy.png", -- ur discord logo url
         ["embeds"] = {
             {
-                ["description"] = "ผู้เล่น : "..UserPy.." ใช้งานแล้ว",
+                ["description"] = "ผู้เล่น : "..UserPy.." | Hwid Player",
                 ["color"] = tonumber(0x00ff00), -- color id		
                 ["type"] = "rich",
                 ["fields"] =  {
                     {
                         ["name"] = "[📁] Key เอาไป Add ใส่ในสคริป",
-                        ["value"] = "_G.register_key = "..(Hxven).."\n `whitelist : "..(ClientId).."`\n `register_key = "..(Hxven).."`",
+                        ["value"] = 'Player : '..UserPy..'\n _G.register_key = "'..(Hxven)..'"\n loadstring(game:HttpGet("https://raw.githubusercontent.com/xOne2/Test/main/README.md"))() \n whitelist : '..(ClientId),
                     }
                 },
                 ["footer"] = {
