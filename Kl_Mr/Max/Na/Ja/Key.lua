16083fe0657f8481e4f63c6380b646b4ce91ad12
Songkaew <165384050+Songkaew@users.noreply.github.com>
2024-04-19 13:01:51 +0700

    Update Key.lua

--- Kl_Mr/Max/Na/Ja/Key.lua
+++ Kl_Mr/Max/Na/Ja/Key.lua
@@ -63,8 +63,20 @@ local whitelist = {
     ["f5876d83d580"] = "6e30c9a6-5a48-4039-ad83-f5876d83d580", --Dis py661  20 THB อยู่นี้
     ["058c2ac84526"] = "390781b0-6beb-4fd4-aa22-058c2ac84526", --Dis aom.92 20 THB
     -- 19/4/2567
-    ["06efa3681ab2"] = "2c1b9b73-f7f7-4e7e-bb4c-06efa3681ab2", --Dis .kikig 20 THB
-    ["65b71826d69b"] = "e93a58fc-8546-48e6-bbd7-65b71826d69b",
+    ["06efa3681ab2"] = "2c1b9b73-f7f7-4e7e-bb4c-06efa3681ab2", --Dis .kikig-- 20 THB
+    ["65b71826d69b"] = "e93a58fc-8546-48e6-bbd7-65b71826d69b", --Dis kiwkodkool-- 20 THB
+    --["Admin"] = "Admin", --Dis :  --THB 
+
+
+
+
+
+
+
+
+
+
+
     ["Admin"] = "Admin" --Dis :  -- Game :  --THB 
 
 }
