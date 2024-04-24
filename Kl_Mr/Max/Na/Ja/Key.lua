d404ad0c43ba15323ba1ade301625d762534fc51
Songkaew <165384050+Songkaew@users.noreply.github.com>
2024-04-24 12:08:51 +0700

    Update Key.lua

--- Kl_Mr/Max/Na/Ja/Key.lua
+++ Kl_Mr/Max/Na/Ja/Key.lua
@@ -31,94 +31,8 @@ local whitelist = string.split((ClientId), '-')
 local Hxven = whitelist[5]
 
 local whitelist = {
-    --["Admin"] = "Admin", --Dis : kkkk -- Game : kkkkkk --THB10
-    ["ba2c2048b81d"] = "3c04d383-7de3-4c10-aec7-ba2c2048b81d", --MrMaxNaJa
-    ["741797658105"] = "8b630005-2003-41f5-84ce-741797658105", --MrMaxNaJa
-    ["8ca7d84a2cc8"] = "fdaba1e9-c20d-4d10-946e-8ca7d84a2cc8", -- Wx
-    ["c80c1c29ca03"] = "c02a7e07-eb73-4f09-8739-c80c1c29ca03", -- Wx --จำลอง
-    ["212bf151f285"] = "151be2d7-d3c4-4bcb-a4a3-212bf151f285", -- MrMaxNaJa and
-
-    ["b7047c99ef36"] = "8758137a-e608-4ab6-b8de-b7047c99ef36", --Dis : park1526 --Game : DorothiezU8748 --THB 10
-    ["2440cdd729c5"] = "6ed89a3c-37a6-469c-a869-2440cdd729c5", --Dis : lilfaxe --Game : FAXEHOPE --THB 10
-    ["903dee7dc1d6"] = "d2d34fbf-768c-431e-9fb4-903dee7dc1d6", --Dis : ddx445 -- Game : 11lfutpbhurv11_23 --THB10
-    ["cdf07ebf23b0"] = "7f9f2621-299c-4acf-98b5-cdf07ebf23b0", --Dis : ameenthailand -- Game : JJShopJQlb --THB10
-    ["9da146e62ef9"] = "a38e323b-f6c0-4a4e-a693-9da146e62ef9", --Dis : ameenthailand.   Game : JJShopJQlb --THB 10 ชื้อ 2
-    --["41ae65f24275"] = "0f70bd65-ea8b-4bf2-80c2-41ae65f24275", --Dis : smokey0792 -- Game : ooloo0_0 --THB10 -- ย้าย 1
-    --["8ad25655c214"] = "1fdd74e5-16e4-4590-9f5a-8ad25655c214", --Dis : smokey0792 -- Game : ooloo0_0 --THB10 คนนี้ลบโหลดตัวรันใหม่เลยสร้างkeyใหม่่
-    --["581575ce67cb"] = "e0a24a6b-dc0d-411b-a79f-581575ce67cb", --Dis ti_it | Game : kritmaidaihackna THB10
-    ["a670ccec9050"] = "35942f62-1d1a-4d77-af4d-a670ccec9050", --Dis ti_it
-    ["69984051992F"] = "1832A2A7-519D-4986-B367-69984051992F", --Dis exousss | Game : demarbec
-    ["a22494a1b7cd"] = "0a8d55c6-74c4-4639-a121-a22494a1b7cd", --Dis somrak_fbi.   Game | : Pro_Somrak THB 10
-    ["24c3da743496"] = "51553654-5dc5-4775-8d1c-24c3da743496", --Dis fun2096  THB10
-    ["b4f081247e3b"] = "dddd761d-0847-4b5e-b445-b4f081247e3b", --Dis : immaculate_eagle_76055 -- Game :  --THB10
-   -- ["f5a54dac5f20"] = "12b1d9cf-7705-4a67-ba5f-f5a54dac5f20", --Dis : w8z6hc -- Game :  --THB20 มันโหลดตัวรันใหม่
-    ["10c5701c5bd4"] = "0fda1e4c-dd41-4a1a-837e-10c5701c5bd4", --Dis w8z6hc อันล่าสุด
-    ["2c24a414fe3f"] = "344ae7e8-68f9-4589-ae00-2c24a414fe3f", --Dis : pobo_2077 -- Game :  --THB20
-    ["df2e5a931a9f"] = "b8487a66-4063-4e1a-af3c-df2e5a931a9f", --Dis : palm123112 -- Game :  --THB20
-    ["b8ee5ed97021"] = "21f4f41a-a763-4da8-a166-b8ee5ed97021", -----Dis : smokey0792 -- Game : ooloo0_0 --THB10 keyใหม่อยู่นี้
-
-    --// วันที่ 18 เดือน 4 ปี 2567 //--
-    --["69bbe39e5ffd"] = "23a6010f-877a-47af-bd98-69bbe39e5ffd", ---Dis py661 THB 20  ลบเกมโหลดใหม่
-    ["13071b00148a"] = "9bb259ce-6822-4eb0-873f-13071b00148a", --Dis plub3757 THB 20 
-    ["fab643351602"] = "433b16b6-f27e-4c39-b3a0-fab643351602", --Dis xxoilkunhgxx THB 20
-    ["e6633977750a"] = "89130351-e264-439a-a825-e6633977750a", --Dis ohmxp0 THB20
-   -- ["af5f08d245ca"] = "e3a9df58-2d36-4b7c-bd69-af5f08d245ca", --Dis nay6267 THB20 เเก้ ลบโหลดใหม่
-    ["f5876d83d580"] = "6e30c9a6-5a48-4039-ad83-f5876d83d580", --Dis py661  20 THB อยู่นี้
-    ["058c2ac84526"] = "390781b0-6beb-4fd4-aa22-058c2ac84526", --Dis aom.92 20 THB
-    -- 19/4/2567
-    ["06efa3681ab2"] = "2c1b9b73-f7f7-4e7e-bb4c-06efa3681ab2", --Dis .kikig 20 THB
-    ["65b71826d69b"] = "e93a58fc-8546-48e6-bbd7-65b71826d69b", -- Dis kiwkodkool 20 THB
-    ["3783c8775e63"] = "100b205d-d413-4183-88ed-3783c8775e63", --Dis rwiphlechuuerd THB 20
-    ["634022b05397"] = "27315009-b0ed-4994-bb75-634022b05397", --Dis godfathercrcr THB 20
-    ["6fbfb2136bed"] = "2469bbd0-dc08-406c-a900-6fbfb2136bed", --Dis wish.x THB 20
-    ["2849d34c00cf"] = "c2ec8c18-954a-4a8e-af94-2849d34c00cf", --Dis asdad.asd THB 20 MrMax - 20 ธนา
-    ["ecf089750b6a"] = "c0c594cd-3c0e-4b55-b628-ecf089750b6a", --Dis achinakub THB 20 
-    ["1a5c226f665b"] = "896f49fc-79ec-46ea-b703-1a5c226f665b", --Dis nay6267 THB 20 เเก้ใหม่
-    ["9315c7fb812d"] = "02908ea9-c865-41e5-84df-9315c7fb812d", --Dis karu02913 THB 20
-    ["0a75ea6b5bf4"] = "6c51c9bd-ab8e-460d-8db6-0a75ea6b5bf4", --Dis rakthaiii 20 THB
-    ["ea0a3cda0320"] = "768ac446-f534-47e6-8d9c-ea0a3cda0320", --Dis thanongtoi_ 20 THB
-    ["17e74ec7689d"] = "ac83a092-c75b-4279-8a93-17e74ec7689d", --Dis .33.0514_27886 20 THB
-    ["27333fb1d71d"] = "f3a5b7ae-6b31-4837-8fcf-27333fb1d71d", --Dis pun37774 20 THB
-    ["88b29271751a"] = "ffc407af-d54a-457c-baaa-88b29271751a", --Dis hszx.15 YT No Buy
-    ["4754ae0453a4"] = "9cfc269a-a31f-49b5-84cd-4754ae0453a4", --Dis pao089355 20 THB
-    --20/4/2567
-    ["bd5afa2eb60a"] = "62b7d5c9-08f9-4f89-bd6d-bd5afa2eb60a", --Dis _wakumi2 30 THB
-    ["f50b3f94d110"] = "26d7bc2a-d737-43f0-86bd-f50b3f94d110", --Dis thnwatrphuukhamsrii 30 THB
-    ["057cd16103d5"] = "2b28d151-a93b-43e5-820a-057cd16103d5", --Dis rimuru1763 30 THB
-    ["6aad3d26ce09"] = "b4f9dfa4-57eb-42a7-8857-6aad3d26ce09", --Dis saran07 30THB
-    ["7ee608ad5090"] = "cd6f5c5f-bbf5-4e93-966f-7ee608ad5090", --Dis stellas_t 30THB ธนาคาร
-    ["ac1e81413d7c"] = "5f5a0fe5-70e4-4b0a-8a98-ac1e81413d7c", --Dis aomqi 30THB
-    --21/4/2567
-    ["7b5d170a5185"] = "ebb983e7-a46a-4158-8a47-7b5d170a5185", --Dis  gleeeeeeeeeeeeee 30 THB
-    ["66e58ba1d394"] = "8777b153-032d-4179-8b24-66e58ba1d394", --Dis itssense 30THB
-    ["978699e05d51"] = "65d1ce06-235e-4bef-9013-978699e05d51", --Dis fhdjnvvb 20 THB  --ติดอยู่10บาท
-    ["63da7f3b7640"] = "ffbbc6b6-68c1-403f-8bb0-63da7f3b7640", --Dis .33.0514_27886 ซื้อเพิ่ม 25 
-    ["c10e6326f5e1"] = "441f6d97-b1d6-49bb-b527-c10e6326f5e1", --Dis noob00_8 30 THB
-    --["cbbc85b226bf"] = "2d91d6d3-10ed-44fd-97e7-cbbc85b226bf", --Dis p_poom082 30 THB
-    ["d2d7a229d152"] = "e5d6dc1a-f499-4e90-9bd7-d2d7a229d152", --Dis thnwatrphuukhamsrii เเก้HWID 15 THB
-    ["083B6C31D6D8"] = "9C671507-19ED-4D14-9107-083B6C31D6D8", --Dis eurosadlhwqofihsad 30 THB
-    ["adcc019c6c93"] = "3be0d390-7b1d-4dce-a931-adcc019c6c93", --Dis daimokin 30 THB
-    --22/4/2567
-    ["af0d8ff3e5b2"] = "05cce1ac-b696-4aa7-8ad2-af0d8ff3e5b2", --Dis p_poom082 15 THB เปลี่ยน Hwid โอนแล้วหารแล้ว 7.5 THB ทำการเมื่อ 1:16
-    ----------------------------------------------------------
-    ["189073ef20df"] = "89dcb8bd-408f-4b13-b7e0-189073ef20df", --Dis ghu9118 30 THB
-    ["9ccb698851f5"] = "4d3e0a57-635e-42d5-b88e-9ccb698851f5", --Dis tew4808 30 THB
-    ["c3ee20062484"] = "d4bd322f-f08b-49fa-b67d-c3ee20062484", --Dis aqj7xc 30 THB
-    ["0b00d07baa01"] = "26e23a07-a925-4004-97cf-0b00d07baa01", --Dis somrak_fbi. 15 THB ย้าย Hwid
-    ["475addf80f0a"] = "debd8212-cb72-4f5f-93bb-475addf80f0a", --Dis ken5574 30 THB
-    ["81f508453f00"] = "55de75aa-dab9-4b41-a038-81f508453f00", -- Dis .33.0514_27886 30 THB ซื้อKeyเพิ่ม
-    --23/4/2567
-    ["b6eac4ce0786"] = "3572f0d0-36a9-4cbf-b91f-b6eac4ce0786", -- Dis sakurakotori 30 THB
-    ["1bdfd5ef076f"] = "b757d483-424c-418e-a9f3-1bdfd5ef076f", -- Dis taelovemayziuziu 30 THB
-    ["9bc8442c7774"] = "4d174f80-199c-4fc8-8d98-9bc8442c7774", -- Dis taelovemayziuziu 30 THB --2 key
-    --["978699e05d51"] = "65d1ce06-235e-4bef-9013-978699e05d51", -- Dis fhdjnvvb 10 THB  --ครบแล้ว
-    ["a097b58e2ed1"] = "c81d3c30-bb8c-4d06-9523-a097b58e2ed1", --Dis trgracemega 30 THB
-    ["eeb1c00cda53"] = "1af75b6b-711b-4bbc-b320-eeb1c00cda53", --Dis isosloobad 30 THB
-    ["17e666b8f704"] = "c924457d-df92-48cd-a701-17e666b8f704", --Dis .3539 30 THB
-    ["c2b3ccc66c40"] = "9825f130-2ad7-4d03-8b46-c2b3ccc66c40", --Dis no7308 30 THB
-    ["1aa2a2273b9e"] = "276f7f48-3d2e-4d0e-a120-1aa2a2273b9e", --Dis thxng525258216 30 THB
-    ["fd9dc5a24dc8"] = "a297163c-7afc-4c84-ac62-fd9dc5a24dc8", --Dis hakurei5027 30 THB
-    ["0e57d52e2407"] = "9cfb9589-945f-4e5c-b5e9-0e57d52e2407", --Dis Wx Freezzzzz
+--- เริ่มใหม่
+    ["bec4a1db00c2"] = "2f35cd08-0f42-4733-b026-bec4a1db00c2", ---Dis fhdjnvvb | 30 THB เกมอัพเดท
     
     ["Admin"] = "Admin" --Dis :  -- Game :  --THB 
 
@@ -128,7 +42,7 @@ local whitelist = {
 
 
 --[[
-_G.register_key = "0e57d52e2407"
+_G.register_key = "bec4a1db00c2"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xOne2/Test/main/README.md"))() 
 
 --]]
