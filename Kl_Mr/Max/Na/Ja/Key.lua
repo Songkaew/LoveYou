c2ebac7c9ee94f69d43e20ed366e33b9c37692b4
Songkaew <165384050+Songkaew@users.noreply.github.com>
2024-05-01 16:57:29 +0700

    Update Key.lua

--- Kl_Mr/Max/Na/Ja/Key.lua
+++ Kl_Mr/Max/Na/Ja/Key.lua
@@ -150,7 +150,7 @@ local whitelist = {
 	["2fed8ea55425"] = "b3718f93-f081-4824-90fe-2fed8ea55425", --Dis stellas_t 40 THB
 	["5df802cbf403"] = "3c62a6e5-0739-40f8-87bc-5df802cbf403", --Dis .3539
 	["8dd5b0e2b814"] = "f86650e2-595e-4318-b616-8dd5b0e2b814", --Dis areyou_chxnxtip06 เกมอัพ
-
+	["2bc56a03afd6"] = "d44fb0df-de02-4dd4-99f0-2bc56a03afd6",
 
 
 
